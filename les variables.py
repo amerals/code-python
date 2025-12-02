3ad74e050a5e01d0274699fd24c7ed4357fedcf5
amerals <amerals2121@yahoo.com>
2025-12-02 18:01:11 +0100

    tout les type des variables

--- les variables.py
+++ les variables.py
@@ -1,4 +1,12 @@
 name = "amir"
 age = 48
-print(name, age)
 score = 20.5
+win = False
+print(name)
+print(age)
+print(score)
+print(win)
+print(type(name))
+print(type(age))
+print(type(score))
+print(type(win))
