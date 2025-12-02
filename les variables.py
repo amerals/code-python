7768332a72447ffb67b9a35a2c472b93fae8deae
amerals <amerals2121@yahoo.com>
2025-12-02 18:46:26 +0100

    fusionner

--- les variables.py
+++ les variables.py
@@ -1,12 +1,11 @@
 name = "amir"
 age = 48
-score = 20.5
+score = 30.5
 win = False
-print(name)
-print(age)
-print(score)
-print(win)
-print(type(name))
-print(type(age))
-print(type(score))
-print(type(win))
+win = True
+score = score + 10
+
+print("name:", name, type(name))
+print("age:", age, type(age))
+print("score:", score, type(score))
+print("win:", win, type(win))
